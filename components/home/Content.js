72445cf4a3543dc2e91ea2e39d0d796d44987049
Fajar Frog <fajarmaftuhfadli@gmail.com>
2022-01-16 14:32:51 +0700

    fix youtube embed atribute

--- components/home/Content.js
+++ components/home/Content.js
@@ -5,62 +5,62 @@ export default function Content() {
     <Section>
       <div className="skew-y-6 flex flex-col items-center ">
         <h2 className="g-h2 g-text-c2 mb-5">VIDEOS</h2>
-        <div class="youtube-video-container mb-4">
+        <div className="youtube-video-container mb-4">
           <iframe
             width="560"
             height="315"
             src="https://www.youtube-nocookie.com/embed/1HyoWT-qvyA"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
         <div className="w-full flex space-x-4 mb-4">
-          <div class="youtube-video-container">
+          <div className="youtube-video-container">
             <iframe
               width="560"
               height="315"
               src="https://www.youtube-nocookie.com/embed/wI2qjqWcZ4Y"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
-          <div class="youtube-video-container">
+          <div className="youtube-video-container">
             <iframe
               width="560"
               height="315"
               src="https://www.youtube-nocookie.com/embed/JKsVkpXvFZI"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         </div>
         <div className="w-full flex space-x-4 mb-4">
-          <div class="youtube-video-container">
+          <div className="youtube-video-container">
             <iframe
               width="560"
               height="315"
               src="https://www.youtube-nocookie.com/embed/5XoOVDHPngM"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
-          <div class="youtube-video-container">
+          <div className="youtube-video-container">
             <iframe
               width="560"
               height="315"
               src="https://www.youtube-nocookie.com/embed/bAtdQsF8hIQ"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         </div>
